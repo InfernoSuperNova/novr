1ce7737e31ca8ce0d4c67684339de6691cdbb716
George Ciobanu <geo.ciobanu@gmail.com>
2026-06-07 10:23:17 -0700

    Reduce small per-frame VR overhead

--- NOVR/VrUi/NOUIManager.cs
+++ NOVR/VrUi/NOUIManager.cs
@@ -45,6 +45,7 @@ public class NOUIManager : NOVRBehaviour
         UIBehaviorPatcher.DoPatching();
         Create<VrUiCursor>(transform);
         ConfigureUiCameras();
+        OnMainCameraChanged(null, APIBus.MainCamera);
     }
 
     protected override void OnSettingChanged()
@@ -55,7 +56,6 @@ public class NOUIManager : NOVRBehaviour
 
     private void Update()
     {
-        ConfigureUiCameras();
         UpdateSmoothedPosition();
     }
     
@@ -94,7 +94,17 @@ public class NOUIManager : NOVRBehaviour
 
     private void OnMainCameraChanged(Camera? previous, Camera? newCam)
     {
-        newCam?.gameObject?.GetComponent<UniversalAdditionalCameraData>()?.cameraStack?.Add(_cockpitHudCamera);
+        if (newCam == null)
+        {
+            return;
+        }
+
+        var cockpitHudCamera = CockpitHudCamera;
+        var cameraStack = newCam.gameObject.GetComponent<UniversalAdditionalCameraData>()?.cameraStack;
+        if (cameraStack != null && !cameraStack.Contains(cockpitHudCamera))
+        {
+            cameraStack.Add(cockpitHudCamera);
+        }
     }
 
     private void ConfigureUiCameras()
